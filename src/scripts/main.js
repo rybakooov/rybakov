5dd1bf8c2b8411afdd0fe9472146a7360c50f289
Alexandr Rybakov <38357811+rybakooov@users.noreply.github.com>
2020-03-11 10:53:33 +0300

    upload

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('hello.js');
+function ready() {
+  
+  var burger = document.querySelector('.header-burger');
+  var burgerText = document.querySelector('.header-burger p');
+  var menu = document.querySelector('.header-linklist');
+  var body = document.querySelector('body');
+  burger.addEventListener('click', function(){
+    menu.classList.toggle('active');
+    burger.classList.toggle('active');
+    body.classList.toggle('overflow');
+    
+    if (menu.classList.contains('active')){
+      burgerText.innerHTML = 'Close'
+    } else {
+      burgerText.innerHTML = 'Contact'
+    }
+  });
+
+}
+
+document.addEventListener("DOMContentLoaded", ready);
